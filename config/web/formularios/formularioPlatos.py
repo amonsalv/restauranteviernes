56f8f9ac1902908f33e1585a011559a177b70ab6
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2024-08-14 21:47:13 +0000

    chore: Update form labels and add validation to formularioPlatos

--- config/web/formularios/formularioPlatos.py
+++ config/web/formularios/formularioPlatos.py
@@ -13,25 +13,30 @@ class FormularioRegistroPlatos(forms.Form):
         widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
         max_length=5,
         required=True,
-        label="Nombre: "
+        label="Nombre del Plato"
     )
     descripcionPlato=forms.CharField(
         widget=forms.Textarea(attrs={"class":"form-control mb-3"}),
         max_length=50,
-        required=True
+        required=True,
+        label="Descripción del Plato"
     )
     fotoPlato=forms.CharField(
         widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
         max_length=200,
-        required=True
+        required=True,
+        label="Foto del Plato"
     )
     precioPlato=forms.CharField(
         widget=forms.NumberInput(attrs={"class":"form-control mb-3"}),
-        required=True
+        required=True,
+        label="Precio del Plato"
     )
     tipoPlato=forms.ChoiceField(
         widget=forms.Select(attrs={"class":"form-control mb-3"}),
-        choices=PLATOS
+        choices=PLATOS,
+        required=True,
+        label="Tipo de Plato"
     )
 
     
